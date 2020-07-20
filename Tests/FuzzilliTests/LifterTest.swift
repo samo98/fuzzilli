6d989661cada62101c813aaad00da34670e5f3c6
Samuel Sládek <sladek.samuel@gmail.com>
2020-07-20 11:02:30 +0000

    CI should fail

--- Tests/FuzzilliTests/LifterTest.swift
+++ Tests/FuzzilliTests/LifterTest.swift
@@ -48,7 +48,7 @@ class LifterTests: XCTestCase {
         }
         b.callFunction(f, withArgs: [b.loadBool(true), b.loadInt(1)])
 
-        let program = b.finalize()
+        let program = b.finish()
 
         let expectedPrettyCode = """
         function v0(v1,v2,v3) {
